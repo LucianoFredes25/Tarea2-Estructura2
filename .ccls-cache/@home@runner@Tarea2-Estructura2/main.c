faf2875e4aac6aea117c2ba19f5b91ab6cc2c8d8
LucianoFredes25 <127423251+LucianoFredes25@users.noreply.github.com>
2023-04-24 22:33:37 +0000

    switch(opcion)
    creada funcion switch que recibira la opcion del menu para asi realizar la operacion que sea necesaria

--- .ccls-cache/@home@runner@Tarea2-Estructura2/main.c
+++ .ccls-cache/@home@runner@Tarea2-Estructura2/main.c
@@ -1,4 +1,5 @@
 #include "list.h"
+#include "hashmap.h"
 #include <assert.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -10,7 +11,6 @@ typedef struct{
 }Item;
 
 typedef struct{
-  char nombre[50];
   int PA;
   int CantidadItems;
   List *listaItems;
@@ -23,10 +23,37 @@ typedef struct{
   int PAux;
 }Accion;
 
+void mostrarMenu() {
+  printf("\n---  Juego?  ---\n");
+  printf("[1] Registrar Jugador.\n");
+  printf("[2] Mostrar perfil de Jugador.\n");
+  printf("[3] Agregar item a Jugador.\n");
+  printf("[4] Eliminar item a Jugador.\n");
+  printf("[5] Agregar puntos de habilidad a Jugador.\n");
+  printf("[6] Mostrar jugadores con un item especifico.\n");
+  printf("[7] Deshacer ultima accion del jugador.\n");
+  printf("[8] Exportar Jugadores.\n");
+  printf("[9] Cargar datos de Jugadores.\n");
+  printf("[10] Salir.\n");
+  printf("Seleccione una opción: ");
+}
+
+//Opcion 1 Registrar jugador
+
+void registrarJugador(HasMap* mapJugadores){
+
+  
+  
+}
 
 int main() {
 
-  int opcion = 10'
+  int opcion = 10;
+  
+  HashMap* mapJugadores = createMap(10000);
+  HashMap* mapItems = createMap(10000);
+
+  
   while (true) {
 
     mostrarMenu();
@@ -34,7 +61,31 @@ int main() {
     scanf("%d", &opcion);
     printf("\n");
     
-    
+    switch(opcion){
+      case 1:
+        RegistrarJugador(mapJugadores);
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+      case 6:
+        break;
+      case 7:
+        break;
+      case 8:
+        break;
+      case 9:
+        break;
+      case 10:
+        break;
+      default:
+        printf("Opcion Invalida, ingrese nueva opcion\n");
+    }
     
   }
 
